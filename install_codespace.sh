79a89a1ab57b4162d4252c765c6655fe80ff3117
shakedzy <shaked@getapril.com>
2023-09-19 13:27:29 +0300

    Update install_codespace.sh

--- install_codespace.sh
+++ install_codespace.sh
@@ -4,6 +4,6 @@ pip install -r requirements.txt
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-446.0.1-linux-x86.tar.gz
 tar -xf google-cloud-cli-446.0.1-linux-x86.tar.gz
 ./google-cloud-sdk/install.sh
-source ???
+source /home/codespace/.bashrc
 ./google-cloud-sdk/bin/gcloud init
 gcloud auth application-default login
