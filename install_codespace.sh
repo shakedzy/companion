66040a9c59c8365b9c551b620e27b237cda4924a
shakedzy <shaked@getapril.com>
2023-09-19 21:50:05 +0300

    fixing empty lines

--- install_codespace.sh
+++ install_codespace.sh
@@ -4,4 +4,5 @@ pip install -r requirements.txt
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-446.0.1-linux-x86.tar.gz
 tar -xf google-cloud-cli-446.0.1-linux-x86.tar.gz
 ./google-cloud-sdk/install.sh
-echo "\nRestart your terminal and run: ./install_codespace2.sh"
+echo ""
+echo "Restart your terminal and run: ./install_codespace2.sh"
--- install_codespace2.sh
+++ install_codespace2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./google-cloud-sdk/bin/gcloud init
 gcloud auth application-default login
-echo "\nCodespace setup complete! Don't forget to add your OpenAI API key as a secret."
+echo ""
+echo "Codespace setup complete! Don't forget to add your OpenAI API key as a secret."
