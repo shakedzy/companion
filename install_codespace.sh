28cbc36fbc3fe55f1135f9d74a5090c1de73bbe8
shakedzy <shaked@getapril.com>
2023-09-20 17:11:29 +0300

    Update install_codespace.sh

--- install_codespace.sh
+++ install_codespace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-apt-get install ffmpeg libavcodec-extra
+sudo apt-get install ffmpeg libavcodec-extra
 pip install -r requirements.txt
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-446.0.1-linux-x86.tar.gz
 tar -xf google-cloud-cli-446.0.1-linux-x86.tar.gz
